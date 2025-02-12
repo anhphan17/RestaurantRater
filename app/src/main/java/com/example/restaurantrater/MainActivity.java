454b60d11ec1f7037723d98b325494c2d227ffc0
anhphan <anhnguyenphan03@gmail.com>
2025-02-12 13:57:19 -0500

    Cancel Save & Rate Buttons done

--- app/src/main/java/com/example/restaurantrater/MainActivity.java
+++ app/src/main/java/com/example/restaurantrater/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.restaurantrater;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Restaurant currentRestaurant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,65 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initSaveButton();
+        initRateButton();
+        initCancelButton();
     }
+
+    private void initSaveButton() {
+        Button saveBtn = findViewById(R.id.mainSaveButton);
+        saveBtn.setOnClickListener(v -> {
+            boolean didSucceed;
+            RestaurantRaterDataSource ds = new RestaurantRaterDataSource(MainActivity.this);
+            try {
+                ds.open();
+
+                if (currentRestaurant.getRestaurantId() == 1) {
+                    didSucceed = ds.insertRestaurant(currentRestaurant);
+                    if (didSucceed) {
+                        int newRestaurantId = ds.getLastRestaurantId();
+                        currentRestaurant.setRestaurantId(newRestaurantId);
+                    }
+                }
+                else {
+                    ds.updateRestaurant(currentRestaurant);
+                }
+                ds.close();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                ds.close();
+            }
+        });
+    }
+
+    private void initRateButton() {
+        Button rateButton = findViewById(R.id.rateButton);
+        rateButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, RateDishActivity.class);
+            intent.putExtra("restaurant_id", currentRestaurant.getRestaurantId());
+            startActivity(intent);
+        });
+    }
+
+    private void initCancelButton() {
+        Button cancelButton = findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(v -> {
+            EditText restaurantNameET = findViewById(R.id.restaurantNameEditText);
+            EditText streetAddressET = findViewById(R.id.editAddress);
+            EditText cityET = findViewById(R.id.editCity);
+            EditText stateET = findViewById(R.id.editState);
+            EditText zipcodeET = findViewById(R.id.editZipcode);
+
+            restaurantNameET.setText("");
+            streetAddressET.setText("");
+            cityET.setText("");
+            stateET.setText("");
+            zipcodeET.setText("");
+        });
+    }
+
+
 }
